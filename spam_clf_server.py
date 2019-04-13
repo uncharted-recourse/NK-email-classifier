75277e23c18573df7489e2fc13449bdaa2e16246
jlgleason <jeffrey.gleason.3@gmail.com>
2019-04-13 13:59:07 -0500

    move model loading to init method

--- spam_clf_server.py
+++ spam_clf_server.py
@@ -37,6 +37,28 @@ DEBUG = True # boolean to specify whether or not print DEBUG information
 #-----
 class NKEmailClassifier(grapevine_pb2_grpc.ClassifierServicer):
 
+    def __init__(self):
+
+        self.maxlen = 200 # length of each sentence
+        self.max_cells = 100 # maximum number of sentences per email
+
+        checkpoint_dir = "deployed_checkpoints/"
+        execution_config=MODEL_OBJECT
+
+        # load specified execution configuration
+        if execution_config is None:
+            raise TypeError
+        Classifier = Simon(encoder={}) # dummy text classifier
+
+        config = Classifier.load_config(execution_config, checkpoint_dir)
+
+        self.encoder = config['encoder']
+        self.checkpoint = config['checkpoint']
+
+        self.model = Classifier.generate_model(self.maxlen, self.max_cells, len(self.encoder.categories),activation='softmax')
+        Classifier.load_weights(self.checkpoint,config,self.model,checkpoint_dir)
+        self.model._make_predict_function()
+
     # Main classify function
     def Classify(self, request, context):
 
@@ -60,75 +82,50 @@ class NKEmailClassifier(grapevine_pb2_grpc.ClassifierServicer):
         # SIMON NK_email_classifier prediction code
         sample_email = input_doc
         start_time = time.time()
+        
         # set important parameters
-        maxlen = 200 # length of each sentence
-        max_cells = 100 # maximum number of sentences per email
         p_threshold = 0.5 # decision boundary
 
         tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-
-        checkpoint_dir = "deployed_checkpoints/"
         
         if(DEBUG):
             print("DEBUG::sample email (whole, then tokenized into sentences):")
             print(sample_email)
         sample_email_sentence = tokenizer.tokenize(sample_email)
-        sample_email_sentence = [elem[-maxlen:] for elem in sample_email_sentence] # truncate
+        sample_email_sentence = [elem[-self.maxlen:] for elem in sample_email_sentence] # truncate
         print(sample_email_sentence)
         all_email_df = pd.DataFrame(sample_email_sentence,columns=['Email 0'])
         
         all_email_df = all_email_df.astype(str)
-        all_email_df = pd.DataFrame.from_records(DataLengthStandardizerRaw(all_email_df,max_cells))
+        all_email_df = pd.DataFrame.from_records(DataLengthStandardizerRaw(all_email_df,self.max_cells))
 
         if(DEBUG):
             print("DEBUG::the final shape is:")
             print(all_email_df.shape)
 
-        raw_data = np.asarray(all_email_df.ix[:max_cells-1,:]) #truncate to max_cells
+        raw_data = np.asarray(all_email_df.ix[:self.max_cells-1,:]) #truncate to max_cells
         raw_data = np.char.lower(np.transpose(raw_data).astype('U'))
 
-        execution_config=MODEL_OBJECT
-
-        # load specified execution configuration
-        if execution_config is None:
-            raise TypeError
-        Classifier = Simon(encoder={}) # dummy text classifier
-
-        config = Classifier.load_config(execution_config, checkpoint_dir)
-
-        encoder = config['encoder']
-        checkpoint = config['checkpoint']
-
-        X = encoder.x_encode(raw_data,maxlen)
-
-        
-        Categories = encoder.categories
-        category_count = len(Categories)
+        X = self.encoder.x_encode(raw_data,self.maxlen)
 
         # orient the user a bit
         if(DEBUG):
             print("DEBUG::CHECKPOINT:")
-            print(checkpoint)
+            print(self.checkpoint)
             print("DEBUG::Categories:")
-            print(Categories)
+            print(self.encoder.categories)
 
-        model = Classifier.generate_model(maxlen, max_cells, category_count,activation='softmax')
-        Classifier.load_weights(checkpoint,config,model,checkpoint_dir)
-        model.compile(loss='categorical_crossentropy',optimizer='adam', metrics=['binary_accuracy'])
-
-        y = model.predict(X)
+        y = self.model.predict(X)
         # discard empty column edge case
         y[np.all(all_email_df.isnull(),axis=0)]=0
 
-        NK_email_result = encoder.reverse_label_encode(y,p_threshold)
+        NK_email_result = self.encoder.reverse_label_encode(y,p_threshold)
         print("Classification result is:")
         print(NK_email_result)
 
         elapsed_time = time.time()-start_time
         print("Total time for classification is : %.2f sec" % elapsed_time)
-        
-        Classifier.clear_session()  # critical for enabling repeated calls of function
-        
+                
         if NK_email_result[0][0]: # empty edge case
             if DOMAIN_OBJECT=='attack':
                 if NK_email_result[0][0][0] != 'friend':
