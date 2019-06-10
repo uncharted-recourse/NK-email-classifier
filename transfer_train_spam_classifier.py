01a962aa2588e8679581f140d66fcb7000946e80
Ubuntu <ubuntu@ip-172-31-26-180.us-east-2.compute.internal>
2019-06-10 18:49:29 +0000

    add latest transfer learning scripts

--- transfer_train_spam_classifier.py
+++ transfer_train_spam_classifier.py
@@ -34,8 +34,8 @@ def LoadJSONLEmails(N=10000000,datapath=None):
         idx = idx+1
         sample_email = ''
         content = json.loads(line)
-        for url in content["urls"]:
-            sample_email += url + ' '
+        #for url in content["urls"]:
+        #    sample_email += url + ' '
         sample_email += content["body"]
         sample_email_sentence = tokenizer.tokenize(sample_email)
         sample_email_sentence = [elem[-maxlen:] for elem in sample_email_sentence] #truncate
@@ -51,8 +51,8 @@ maxlen = 200 # max length of each sentence
 max_cells = 100 # maximum number of sentences per email
 p_threshold = 0.5 # decision boundary
 batch_size = 32
-nb_epoch = 40
-
+nb_epoch = 10
+'''
 # Extract dry run historical data from jsonl format
 datapath = "dry_run_data/historical_chris.jsonl"
 chris_data = LoadJSONLEmails(datapath=datapath)
@@ -64,32 +64,67 @@ datapath = "dry_run_data/historical_paul.jsonl"
 paul_data = LoadJSONLEmails(datapath=datapath)
 datapath = "dry_run_data/historical_wayne.jsonl"
 wayne_data = LoadJSONLEmails(datapath=datapath)
-datapath = "dry_run_data/recourse-attacks.jsonl"
-attack_data = LoadJSONLEmails(datapath=datapath)
+
+N_spam = 150 # number of samples to draw
+datapath = "data/nigerian.jsonl"
+nigerian_prince = LoadJSONLEmails(N=N_spam,datapath=datapath)
+datapath = "data/Malware.jsonl"
+malware = LoadJSONLEmails(N=N_spam,datapath=datapath)
+datapath = "data/CredPhishing.jsonl"
+credphishing = LoadJSONLEmails(N=N_spam,datapath=datapath)
+datapath = "data/PhishTraining.jsonl"
+phishtraining = LoadJSONLEmails(N=N_spam,datapath=datapath)
+datapath = "data/Propaganda.jsonl"
+propaganda = LoadJSONLEmails(N=N_spam,datapath=datapath)
+datapath = "data/SocialEng.jsonl"
+socialeng = LoadJSONLEmails(N=N_spam,datapath=datapath)
+datapath = "data/Spam.jsonl"
+spam = LoadJSONLEmails(N=N_spam,datapath=datapath)
 
 # keep dataset approximately balanced
-raw_data = np.asarray(chris_data.ix[:max_cells-1,:])
-header = [['friend'],]*chris_data.shape[0]
+raw_data = np.asarray(nigerian_prince.ix[:max_cells-1,:])
+header = [['foe'],]*N_spam
+print(raw_data.shape)
+raw_data = np.column_stack((raw_data,np.asarray(malware.ix[:max_cells-1,:])))
+header.extend([['foe'],]*N_spam)
+print(raw_data.shape)
+raw_data = np.column_stack((raw_data,np.asarray(credphishing.ix[:max_cells-1,:])))
+header.extend([['foe'],]*N_spam)
+print(raw_data.shape)
+print(phishtraining.shape)
+raw_data = np.column_stack((raw_data,np.asarray(phishtraining.ix[:max_cells-1,:])))
+header.extend([['foe'],]*N_spam)
+print(raw_data.shape)
+raw_data = np.column_stack((raw_data,np.asarray(propaganda.ix[:max_cells-1,:])))
+header.extend([['foe'],]*N_spam)
+print(raw_data.shape)
+raw_data = np.column_stack((raw_data,np.asarray(socialeng.ix[:max_cells-1,:])))
+header.extend([['foe'],]*N_spam)
+print(raw_data.shape)
+raw_data = np.column_stack((raw_data,np.asarray(spam.ix[:max_cells-1,:])))
+header.extend([['foe'],]*N_spam)
+
+# keep dataset approximately balanced
+raw_data = np.column_stack((raw_data,np.asarray(chris_data.ix[:max_cells-1,:])))
+header.extend([['friend'],]*chris_data.shape[1])
 print(raw_data.shape)
 raw_data = np.column_stack((raw_data,np.asarray(christine_data.ix[:max_cells-1,:])))
-header.extend([['friend'],]*christine_data.shape[0])
+header.extend([['friend'],]*christine_data.shape[1])
 print(raw_data.shape)
 raw_data = np.column_stack((raw_data,np.asarray(ian_data.ix[:max_cells-1,:])))
-header.extend([['friend'],]*ian_data.shape[0])
+header.extend([['friend'],]*ian_data.shape[1])
 print(raw_data.shape)
 raw_data = np.column_stack((raw_data,np.asarray(paul_data.ix[:max_cells-1,:])))
-header.extend([['friend'],]*paul_data.shape[0])
+header.extend([['friend'],]*paul_data.shape[1])
 print(raw_data.shape)
 raw_data = np.column_stack((raw_data,np.asarray(wayne_data.ix[:max_cells-1,:])))
-header.extend([['friend'],]*wayne_data.shape[0])
+header.extend([['friend'],]*wayne_data.shape[1])
 print(raw_data.shape)
-raw_data = np.column_stack((raw_data,np.asarray(attack_data.ix[:max_cells-1,:])))
-header.extend([['foe'],]*attack_data.shape[0])
 
 print("DEBUG::final labeled data shape:")
 print(raw_data.shape)
 print(raw_data)
-
+raw_data = np.char.lower(np.transpose(raw_data).astype('U'))
 
 # transpose the data, make everything lower case string
 mini_batch = 1000 # because of some memory issues, the next step needs to be done in stages
@@ -110,19 +145,18 @@ header = tmp_header
 end = time.time()
 print("Time for casting data as lower case string is %f sec"%(end-start))
 raw_data = tmp
-
 # save data for future experiments
 f = open('historical_raw_data.npy', 'wb')
 np.save(f, raw_data)
 f = open('historical_header.npy', 'wb')
 np.save(f, header)
-
+'''
 # load data 
-#raw_data = np.load('raw_data', allow_pickle=True)
-#header = np.load('header', allow_pickle=True)
+raw_data = np.load('historical_raw_data.npy', allow_pickle=True)
+header = np.load('historical_header.npy', allow_pickle=True)
 
 # load checkpoint (encoder with categories, weights)
-modelName = ''
+modelName = 'text-class.17-0.14.pkl'
 checkpoint_dir = "checkpoints/"
 config = Simon({}).load_config(modelName,checkpoint_dir)
 encoder = config['encoder']
@@ -176,4 +210,3 @@ file.open('experiment_metrics.txt', 'a')
 file.write("baseline classifier with urls: %0.3f\n" % (history.history['val_binary_accuracy']))
 file.flush()
 file.close()
-
