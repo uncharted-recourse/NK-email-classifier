632b5a6970c88752a224b297293ddf6c3323724a
jgleason <jeffrey.gleason@newknowledge.io>
2019-08-07 13:27:22 -0500

    update training and transfer training scripts

--- train_spam_classifier.py
+++ train_spam_classifier.py
@@ -10,28 +10,50 @@ from Simon import Simon
 from Simon.Encoder import Encoder
 from Simon.DataGenerator import DataGenerator
 from Simon.LengthStandardizer import *
-from sklearn.utils.class_weight import compute_class_weight
 
-# extract the first N samples from jsonl
-def LoadJSONLEmails(datapath=None):
+def LoadJSONLEmails(N = 50000, datapath=None):
     tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+    start = time.time()
     with open(datapath) as data_file:
-        data_JSONL_lines = data_file.readlines()
-    sample_email = json.loads(data_JSONL_lines[idx])["body"]
+        data_JSONL_lines = data_file.readlines()[:N]
     print("DEBUG::the current email type being loaded:")
     print(datapath)
-    content = [json.loads(line) for line in data_JSONL_lines]
-    content = [tokenizer.tokenize([u + ' ' for u in c['urls']].append(c['body'])[0]) for c in content]
-    content = [c[-maxlen:] for c in content]
-    all_email_df = pd.DataFrame(content).transpose()
-    return pd.DataFrame.from_records(DataLengthStandardizerRaw(all_email_df,max_cells))
+    emails = [json.loads(line) for line in data_JSONL_lines]
+    print('Content Loading {} took {} seconds'.format(datapath, time.time() - start))
+    lines = []
+    for email in emails:
+        txt = ''
+        for url in email['urls']:
+            txt += url + '.\n'
+        txt += email['subject'] + '.\n'
+        txt += email['body']
+        sentences = [sentence[:maxlen] for sentence in tokenizer.tokenize(txt)][:max_cells]
+        if len(sentences) == 0:
+            continue
+        none_count = max_cells - len(sentences)
+        sentences = np.pad(sentences, (0,none_count), mode='wrap')
+        lines.append(sentences)
+    print('Parsing Loading {} took {} seconds'.format(datapath, time.time() - start))
+    return np.transpose(lines)
+
+def prepare_data(datapaths, labels):
+    if raw_data is None:
+        data = np.column_stack([LoadJSONLEmails(datapath=p) for p in datapaths])
+    else:
+        data = np.column_stack([LoadJSONLEmails(datapath=p) for p in datapaths])
+        raw_data = np.column_stack((raw_data, data))
+    if len(labels) == 1:
+        header.extend([labels] * data.shape[1])
+    else:
+        header.extend(labels)
+    return raw_data, header
 
 # set important parameters
 maxlen = 200 # max length of each sentence
 max_cells = 100 # maximum number of sentences per email
 p_threshold = 0.5 # decision boundary
 
-ham_datapaths = ["data/enron.jsonl", 
+ham_datapaths = ["data/enron.jsonl",
                 "dry_run_data/historical_chris.jsonl",
                 "dry_run_data/historical_christine.jsonl",
                 "dry_run_data/historical_ian.jsonl",
@@ -48,70 +70,84 @@ spam_datapaths = ["data/nigerian.jsonl",
                 "ta3-attacks/ta3-june-campaign.jsonl",
                 "ta3-attacks/ta3-july-campaign.jsonl"]
 
-ham_data = [np.asarray(LoadJSONLEmails(datapath=h).ix[:max_cells-1,:]) for h in ham_datapaths]
-spam_data = [np.asarray(LoadJSONLEmails(datapath=h).ix[:max_cells-1,:]) for h in spam_datapaths]
-raw_data_ham = np.column_stack(ham_data)
-raw_data_spam = np.column_stack(spam_data)
-header = [['friend'],] * raw_data_ham.shape[0]
-header.extend([['foe'],] * raw_data_spam.shape[0])
-raw_data = np.column_stack(raw_data_ham, raw_data_spam)
+# header = []
+# raw_data = None
+# raw_data, header = prepare_data(ham_datapaths, ['friend'])
+# raw_data, header = prepare_data(spam_datapaths, ['foe'])
 
-print("DEBUG::final labeled data shape:")
-print(raw_data.shape)
-print(raw_data)
-
-# transpose the data, make everything lower case string
-mini_batch = 1000 # because of some memory issues, the next step needs to be done in stages
-start = time.time()
-tmp = np.char.lower(np.transpose(raw_data[:,:mini_batch]).astype('U'))
-tmp_header = header[:mini_batch]
-for i in range(1,int(raw_data.shape[1]/mini_batch)):
-    try:
-        tmp = np.vstack((tmp,np.char.lower(np.transpose(raw_data[:,i*mini_batch:(i+1)*mini_batch]).astype('U'))))
-        tmp_header.extend(header[i*mini_batch:(i+1)*mini_batch])
-    except:
-        print("failed string standardization on batch number "+str(i))
-
-header = tmp_header
-end = time.time()
-print("Time for casting data as lower case string is %f sec"%(end-start))
-raw_data = tmp
+# # transpose the data, make everything lower case string
+# raw_data = np.char.lower(np.transpose(raw_data).astype('U'))
 
 # save data for future experiments
-f = open('raw_data', 'wb')
-np.save(f, raw_data)
-f.close()
-f = open('header', 'wb')
-np.save(f, header)
-f.close()
+# f = open('header', 'wb')
+# np.save(f, header)
+# f.close()
+# f = open('raw_data', 'wb')
+# np.save(f, raw_data)
+# f.close()
 
-# load data 
-#raw_data = np.load('raw_data.npy', allow_pickle=True)
-#header = np.load('header.npy', allow_pickle=True)
+header = np.load('header', allow_pickle=True)
+raw_data = np.load('raw_data', allow_pickle=True)
 
 # set up appropriate data encoder
 Categories = ['friend','foe']
+header = np.load('header', allow_pickle=True)
+raw_data = np.load('raw_data', allow_pickle=True)
 encoder = Encoder(categories=Categories)
 encoder.process(raw_data, max_cells)
 # encode the data 
 X, y, class_weights = encoder.encode_data(raw_data, header, maxlen)
+print(class_weights)
+print(X.shape)
+print(y.shape)
+
 # setup classifier, compile model appropriately
 Classifier = Simon(encoder=encoder)
 data = Classifier.setup_test_sets(X, y)
 model = Classifier.generate_model(maxlen, max_cells, 2,activation='softmax')
 model.compile(loss='categorical_crossentropy',optimizer='adam', metrics=['binary_accuracy'])
+
 # train model
 checkpoint_dir = "checkpoint_ta3_attack/"
 if not os.path.isdir(checkpoint_dir):
     os.makedirs(checkpoint_dir)
 start = time.time()
-history = Classifier.train_model(model, data, checkpoint_dir, class_weight=class_weights)
+history = Classifier.train_model(model, data, checkpoint_dir, class_weight=class_weights, batch_size=24)
 end = time.time()
 print("Time for training is %f sec"%(end-start))
 config = { 'encoder' :  encoder,
             'checkpoint' : Classifier.get_best_checkpoint(checkpoint_dir) }
 Classifier.save_config(config, checkpoint_dir)
-Classifier.evaluate_model(max_cells, model, data, encoder, p_threshold, checkpoint_dir = checkpoint_dir)
+
+# evaluate model
+test_friend_datapaths = ["dry_run_data/chris.jsonl",
+                "dry_run_data/christine.jsonl",
+                "dry_run_data/ian.jsonl",
+                "dry_run_data/paul.jsonl",
+                "dry_run_data/wayne.jsonl"]
+test_foe_datapaths = ["dry_run_data/recourse-attacks.jsonl"]
+
+test_header = []
+test_raw_data = None
+test_raw_data, test_header = prepare_data(test_friend_datapaths, ['friend'])
+test_raw_data, test_header = prepare_data(test_foe_datapaths, ['foe'])
+
+# transpose the data, make everything lower case string
+test_raw_data = np.char.lower(np.transpose(raw_data).astype('U'))
+
+# save data for future experiments
+f = open('test_header', 'wb')
+np.save(f, test_header)
+f.close()
+f = open('test_raw_data', 'wb')
+np.save(f, test_raw_data)
+f.close()
+
+# test_header = np.load('test_header', allow_pickle=True)
+# test_raw_data = np.load('test_raw_data', allow_pickle=True)
+
+test_data = type('data_type',(object,),{'X_test':test_raw_data,'y_test':test_header})
+Classifier.evaluate_model(max_cells, model, test_data, encoder, p_threshold, checkpoint_dir = checkpoint_dir)
 
 '''
 # do p_threshold ROC tuning on the test data to see if you can improve it
--- transfer_train_spam_classifier.py
+++ transfer_train_spam_classifier.py
@@ -11,149 +11,83 @@ from Simon.Encoder import Encoder
 from Simon.DataGenerator import DataGenerator
 from Simon.LengthStandardizer import *
 
-# extract the first N samples from jsonl
-def LoadJSONLEmails(N=10000000,datapath=None):
+def LoadJSONLEmails(N = 50000, datapath=None):
     tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+    start = time.time()
     with open(datapath) as data_file:
-        data_JSONL_lines = data_file.readlines()
-    random.shuffle(data_JSONL_lines)
-    # visualize body extraction for first email
-    idx = 0
-    sample_email = json.loads(data_JSONL_lines[idx])["body"]
+        data_JSONL_lines = data_file.readlines()[:N]
     print("DEBUG::the current email type being loaded:")
     print(datapath)
-    #print("DEBUG::sample email (whole, then tokenized into sentences):")
-    #print(sample_email)
-    sample_email_sentence = tokenizer.tokenize(sample_email)
-    sample_email_sentence = [elem[-maxlen:] for elem in sample_email_sentence] # truncate
-    #print(sample_email_sentence)
-    all_email_df = pd.DataFrame(sample_email_sentence,columns=['Email 0'])
-    # now, build up pandas dataframe of appropriate format for NK email classifier
-    for line in data_JSONL_lines:
-        print(idx)
-        idx = idx+1
-        sample_email = ''
-        content = json.loads(line)
-        #for url in content["urls"]:
-        #    sample_email += url + ' '
-        sample_email += content["body"]
-        sample_email_sentence = tokenizer.tokenize(sample_email)
-        sample_email_sentence = [elem[-maxlen:] for elem in sample_email_sentence] #truncate
-        all_email_df = pd.concat([all_email_df,pd.DataFrame(sample_email_sentence,columns=['Email '+str(idx)])],axis=1)
-        if idx>=N-1:
-            break
+    emails = [json.loads(line) for line in data_JSONL_lines]
+    print('Content Loading {} took {} seconds'.format(datapath, time.time() - start))
+    lines = []
+    for email in emails:
+        txt = ''
+        for url in email['urls']:
+            txt += url + '.\n'
+        txt += email['subject'] + '.\n'
+        txt += email['body']
+        sentences = [sentence[:maxlen] for sentence in tokenizer.tokenize(txt)][:max_cells]
+        if len(sentences) == 0:
+            continue
+        none_count = max_cells - len(sentences)
+        sentences = np.pad(sentences, (0,none_count), mode='wrap')
+        lines.append(sentences)
+    print('Parsing Loading {} took {} seconds'.format(datapath, time.time() - start))
+    return np.transpose(lines)
 
-    return pd.DataFrame.from_records(DataLengthStandardizerRaw(all_email_df,max_cells))
-
+def prepare_data(datapaths, labels):
+    if raw_data is None:
+        data = np.column_stack([LoadJSONLEmails(datapath=p) for p in datapaths])
+    else:
+        data = np.column_stack([LoadJSONLEmails(datapath=p) for p in datapaths])
+        raw_data = np.column_stack((raw_data, data))
+    if len(labels) == 1:
+        header.extend([labels] * data.shape[1])
+    else:
+        header.extend(labels)
+    return raw_data, header
 
 # set important parameters
 maxlen = 200 # max length of each sentence
 max_cells = 100 # maximum number of sentences per email
 p_threshold = 0.5 # decision boundary
-batch_size = 32
-nb_epoch = 10
-'''
-# Extract dry run historical data from jsonl format
-datapath = "dry_run_data/historical_chris.jsonl"
-chris_data = LoadJSONLEmails(datapath=datapath)
-datapath = "dry_run_data/historical_christine.jsonl"
-christine_data = LoadJSONLEmails(datapath=datapath)
-datapath = "dry_run_data/historical_ian.jsonl"
-ian_data = LoadJSONLEmails(datapath=datapath)
-datapath = "dry_run_data/historical_paul.jsonl"
-paul_data = LoadJSONLEmails(datapath=datapath)
-datapath = "dry_run_data/historical_wayne.jsonl"
-wayne_data = LoadJSONLEmails(datapath=datapath)
 
-N_spam = 150 # number of samples to draw
-datapath = "data/nigerian.jsonl"
-nigerian_prince = LoadJSONLEmails(N=N_spam,datapath=datapath)
-datapath = "data/Malware.jsonl"
-malware = LoadJSONLEmails(N=N_spam,datapath=datapath)
-datapath = "data/CredPhishing.jsonl"
-credphishing = LoadJSONLEmails(N=N_spam,datapath=datapath)
-datapath = "data/PhishTraining.jsonl"
-phishtraining = LoadJSONLEmails(N=N_spam,datapath=datapath)
-datapath = "data/Propaganda.jsonl"
-propaganda = LoadJSONLEmails(N=N_spam,datapath=datapath)
-datapath = "data/SocialEng.jsonl"
-socialeng = LoadJSONLEmails(N=N_spam,datapath=datapath)
-datapath = "data/Spam.jsonl"
-spam = LoadJSONLEmails(N=N_spam,datapath=datapath)
+ham_datapaths = ["data/enron.jsonl",
+                "dry_run_data/historical_chris.jsonl",
+                "dry_run_data/historical_christine.jsonl",
+                "dry_run_data/historical_ian.jsonl",
+                "dry_run_data/historical_paul.jsonl",
+                "dry_run_data/historical_wayne.jsonl"]
+spam_datapaths = ["data/nigerian.jsonl",
+                "data/Malware.jsonl",
+                "data/CredPhishing.jsonl",
+                "data/PhishTraining.jsonl",
+                "data/Propaganda.jsonl",
+                "data/SocialEng.jsonl",
+                "data/Spam.jsonl",
+                "ta3-attacks/ta3-may-campaign.jsonl",
+                "ta3-attacks/ta3-june-campaign.jsonl",
+                "ta3-attacks/ta3-july-campaign.jsonl"]
 
-# keep dataset approximately balanced
-raw_data = np.asarray(nigerian_prince.ix[:max_cells-1,:])
-header = [['foe'],]*N_spam
-print(raw_data.shape)
-raw_data = np.column_stack((raw_data,np.asarray(malware.ix[:max_cells-1,:])))
-header.extend([['foe'],]*N_spam)
-print(raw_data.shape)
-raw_data = np.column_stack((raw_data,np.asarray(credphishing.ix[:max_cells-1,:])))
-header.extend([['foe'],]*N_spam)
-print(raw_data.shape)
-print(phishtraining.shape)
-raw_data = np.column_stack((raw_data,np.asarray(phishtraining.ix[:max_cells-1,:])))
-header.extend([['foe'],]*N_spam)
-print(raw_data.shape)
-raw_data = np.column_stack((raw_data,np.asarray(propaganda.ix[:max_cells-1,:])))
-header.extend([['foe'],]*N_spam)
-print(raw_data.shape)
-raw_data = np.column_stack((raw_data,np.asarray(socialeng.ix[:max_cells-1,:])))
-header.extend([['foe'],]*N_spam)
-print(raw_data.shape)
-raw_data = np.column_stack((raw_data,np.asarray(spam.ix[:max_cells-1,:])))
-header.extend([['foe'],]*N_spam)
+# header = []
+# raw_data = None
+# raw_data, header = prepare_data(ham_datapaths, ['friend'])
+# raw_data, header = prepare_data(spam_datapaths, ['foe'])
 
-# keep dataset approximately balanced
-raw_data = np.column_stack((raw_data,np.asarray(chris_data.ix[:max_cells-1,:])))
-header.extend([['friend'],]*chris_data.shape[1])
-print(raw_data.shape)
-raw_data = np.column_stack((raw_data,np.asarray(christine_data.ix[:max_cells-1,:])))
-header.extend([['friend'],]*christine_data.shape[1])
-print(raw_data.shape)
-raw_data = np.column_stack((raw_data,np.asarray(ian_data.ix[:max_cells-1,:])))
-header.extend([['friend'],]*ian_data.shape[1])
-print(raw_data.shape)
-raw_data = np.column_stack((raw_data,np.asarray(paul_data.ix[:max_cells-1,:])))
-header.extend([['friend'],]*paul_data.shape[1])
-print(raw_data.shape)
-raw_data = np.column_stack((raw_data,np.asarray(wayne_data.ix[:max_cells-1,:])))
-header.extend([['friend'],]*wayne_data.shape[1])
-print(raw_data.shape)
+# # transpose the data, make everything lower case string
+# raw_data = np.char.lower(np.transpose(raw_data).astype('U'))
 
-print("DEBUG::final labeled data shape:")
-print(raw_data.shape)
-print(raw_data)
-raw_data = np.char.lower(np.transpose(raw_data).astype('U'))
-
-# transpose the data, make everything lower case string
-mini_batch = 1000 # because of some memory issues, the next step needs to be done in stages
-start = time.time()
-tmp = np.char.lower(np.transpose(raw_data[:,:mini_batch]).astype('U'))
-tmp_header = header[:mini_batch]
-for i in range(1,int(raw_data.shape[1]/mini_batch)):
-    print("DEBUG::current shape of loaded text (data,header)")
-    print(tmp.shape)
-    print(len(tmp_header))
-    try:
-        tmp = np.vstack((tmp,np.char.lower(np.transpose(raw_data[:,i*mini_batch:(i+1)*mini_batch]).astype('U'))))
-        tmp_header.extend(header[i*mini_batch:(i+1)*mini_batch])
-    except:
-        print("failed string standardization on batch number "+str(i))
-
-header = tmp_header
-end = time.time()
-print("Time for casting data as lower case string is %f sec"%(end-start))
-raw_data = tmp
 # save data for future experiments
-f = open('historical_raw_data.npy', 'wb')
-np.save(f, raw_data)
-f = open('historical_header.npy', 'wb')
-np.save(f, header)
-'''
-# load data 
-raw_data = np.load('historical_raw_data.npy', allow_pickle=True)
-header = np.load('historical_header.npy', allow_pickle=True)
+# f = open('header', 'wb')
+# np.save(f, header)
+# f.close()
+# f = open('raw_data', 'wb')
+# np.save(f, raw_data)
+# f.close()
+
+header = np.load('header', allow_pickle=True)
+raw_data = np.load('raw_data', allow_pickle=True)
 
 # load checkpoint (encoder with categories, weights)
 modelName = 'text-class.17-0.14.pkl'
@@ -177,19 +111,11 @@ model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['binary_acc
 # print all layers to make sure it is right
 print("DEBUG::total number of layers:")
 print(len(model.layers))
-print("DEBUG::They are:")
-for layer in model.layers:
-    print(layer)
-print("DEBUG::model.summary() is:")
-print(model.summary())
 
 # encode the data and evaluate model
 X, y = encoder.encode_data(raw_data, header, maxlen)
-
 data = Classifier.setup_test_sets(X, y)
-
 max_cells = encoder.cur_max_cells
-
 start = time.time()
 history = Classifier.train_model(batch_size, checkpoint_dir, model, nb_epoch, data)
 end = time.time()
@@ -198,15 +124,33 @@ print("Time for training is %f sec"%(end-start))
 config = { 'encoder' :  encoder,
             'checkpoint' : Classifier.get_best_checkpoint(checkpoint_dir) }
 Classifier.save_config(config, checkpoint_dir)
-Classifier.plot_loss(history) #comment out on docker images...
-    
-Classifier.evaluate_model(max_cells, model, data, encoder, p_threshold)
 
-# write evaluation metrics to file for comparison
-if not os.path.exists('experiment_metrics.txt'):
-     file = open('experiment_metrics.txt', 'w')
-     file.close()
-file.open('experiment_metrics.txt', 'a')
-file.write("baseline classifier with urls: %0.3f\n" % (history.history['val_binary_accuracy']))
-file.flush()
-file.close()
+# evaluate model
+test_friend_datapaths = ["dry_run_data/chris.jsonl",
+                "dry_run_data/christine.jsonl",
+                "dry_run_data/ian.jsonl",
+                "dry_run_data/paul.jsonl",
+                "dry_run_data/wayne.jsonl"]
+test_foe_datapaths = ["dry_run_data/recourse-attacks.jsonl"]
+
+# test_header = []
+# test_raw_data = None
+# test_raw_data, test_header = prepare_data(test_friend_datapaths, ['friend'])
+# test_raw_data, test_header = prepare_data(test_foe_datapaths, ['foe'])
+
+# # transpose the data, make everything lower case string
+# test_raw_data = np.char.lower(np.transpose(raw_data).astype('U'))
+
+# save data for future experiments
+# f = open('test_header', 'wb')
+# np.save(f, test_header)
+# f.close()
+# f = open('test_raw_data', 'wb')
+# np.save(f, test_raw_data)
+# f.close()
+
+test_header = np.load('test_header', allow_pickle=True)
+test_raw_data = np.load('test_raw_data', allow_pickle=True)
+
+test_data = type('data_type',(object,),{'X_test':test_raw_data,'y_test':test_header})    
+Classifier.evaluate_model(max_cells, model, test_data, encoder, p_threshold)
